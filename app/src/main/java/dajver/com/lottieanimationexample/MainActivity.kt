85e0bbf83f99eeaef0c89fc2359f0a0dabaf14bd
gleb <lfqdth5@gmail.com>
2019-07-03 14:59:49 +0300

    first commit

---
+++ app/src/main/java/dajver/com/lottieanimationexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package dajver.com.lottieanimationexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        animationView.setOnClickListener {
+            animationView.playAnimation()
+        }
+    }
+}
